cd7cc0a3011d5c75f0d30eb73f64d1f955411a2f
dfx172 <dfx172@gmail.com>
2022-09-18 23:50:58 -0500

    creados usarios y clave

--- JS/login.js
+++ JS/login.js
@@ -3,8 +3,11 @@ const password = document.getElementById("password");
 const check = document.getElementById("check");
 const spinner = document.getElementById("spinner");
 const button = document.getElementById("connect");
+const form = document.getElementById("form-login");
 const a=12345;
-const b="a1728@";
+const b=54321;
+const c="a1728@";
+
 console.log("script cargado");
 console.log(username.value);
 var username_valid=false;
@@ -13,14 +16,16 @@ var password_valid=false;
 username.oninput=function(){
     if(this.value==a){
         username_valid=true;
-    }else{
-        username_valid=false;
+        form.action="./HTML/informacion.html";
+    }else if(this.value==b){
+        form.action="./HTML/registro.html";
+        }else{username_valid=false;
     }
 form_valid(username_valid,password_valid);
 }
 
 password.oninput=function(){
-    if(this.value==b){
+    if(this.value==c){
         password_valid=true;
     }else{
         password_valid=false;
